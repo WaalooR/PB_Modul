80f2f873b0f7691703b3699e88319b380caab117
birosbeni <Beni>
2023-05-16 16:24:59 +0200

    Add database and modul functionality

--- module-helloworld/Dnn.PirosBiros.HelloWorld/Models/Item.cs
+++ module-helloworld/Dnn.PirosBiros.HelloWorld/Models/Item.cs
@@ -18,57 +18,18 @@ using System.Web.Caching;
 
 namespace PirosBiros.Dnn.Dnn.PirosBiros.HelloWorld.Models
 {
-    [TableName("Dnn.PirosBiros.HelloWorld_Items")]
+    [TableName("Currency")]
     //setup the primary key for table
-    [PrimaryKey("ItemId", AutoIncrement = true)]
+    [PrimaryKey("id", AutoIncrement = true)]
     //configure caching using PetaPoco
     [Cacheable("Items", CacheItemPriority.Default, 20)]
     //scope the objects to the ModuleId of a module on a page (or copy of a module on a page)
     [Scope("ModuleId")]
     public class Item
     {
-        ///<summary>
-        /// The ID of your object with the name of the ItemName
-        ///</summary>
-        public int ItemId { get; set; } = -1;
-        ///<summary>
-        /// A string with the name of the ItemName
-        ///</summary>
-        public string ItemName { get; set; }
-
-        ///<summary>
-        /// A string with the description of the object
-        ///</summary>
-        public string ItemDescription { get; set; }
-
-        ///<summary>
-        /// An integer with the user id of the assigned user for the object
-        ///</summary>
-        public int AssignedUserId { get; set; }
-
-        ///<summary>
-        /// The ModuleId of where the object was created and gets displayed
-        ///</summary>
-        public int ModuleId { get; set; }
-
-        ///<summary>
-        /// An integer for the user id of the user who created the object
-        ///</summary>
-        public int CreatedByUserId { get; set; } = -1;
-
-        ///<summary>
-        /// An integer for the user id of the user who last updated the object
-        ///</summary>
-        public int LastModifiedByUserId { get; set; } = -1;
-
-        ///<summary>
-        /// The date the object was created
-        ///</summary>
-        public DateTime CreatedOnDate { get; set; } = DateTime.UtcNow;
-
-        ///<summary>
-        /// The date the object was updated
-        ///</summary>
-        public DateTime LastModifiedOnDate { get; set; } = DateTime.UtcNow;
+        public int Id { get; set; }
+        public string CurrencyName { get; set; }
+        public string LongCurrencyName { get; set; }
+        public double CurrencyValue { get; set; }
     }
 }
